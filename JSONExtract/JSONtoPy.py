26dd53228094e6015704c33472f8820b007af1ad
Ryan <ryanriyas123@gmail.com>
2026-05-05 00:05:37 +0400

    Update: Fixed the patterns and removed zip()

--- JSONExtract/JSONtoPy.py
+++ JSONExtract/JSONtoPy.py
@@ -1,22 +1,21 @@
 import json
 import re
-from ExcelFunctions.OpenExcel import get_column_names
-from LLMFunctions.LLMResponse import llm_response
 
 def extract_json(llm_output):
     response = llm_output
     jsondict = json.loads(response)
 
-    list_of_patterns = [r"comp", r"gender", r"marital", r"nationality", 
-                        r"visit date", r"visit time", r"temperature", r"pulse", 
-                        r"respiratory", r"bp", r"o2", r"pain scale", r"gcs", 
+    list_of_patterns = [r"age", r"marital", r"nationality", r"date_of_birth",
+                        r"visit_date", r"visit_time", r"temperature", r"pulse", 
+                        r"respiratory", r"bp", r"o2", r"pain_scale", r"gcs", 
                         r"height", r"weight", r"bmi", r"triage", r"chief", 
                         r"past", r"travel", r"medication", r"psychosocial", 
                         r"occupation", r"primary", r"other", r"disease", 
-                        r"remarks", r"disposition time", r"advice", r"condition", 
-                        r"disposition date", r"disposition time"]
+                        r"remarks", r"disposition_type", r"advice", r"condition", 
+                        r"disposition_date", r"disposition_time"]
     values = []
-    for variable,pattern, col in zip(jsondict.keys(),list_of_patterns, get_column_names()):
-        if re.search(pattern=pattern, string=variable) and re.search(pattern=pattern,string=col):
-            values.append(jsondict[variable])
-    yield values
+    for variable in jsondict.keys():
+        for pattern in list_of_patterns:
+            if re.search(pattern=pattern, string=variable):
+                values.append(jsondict[variable])
+    return values
