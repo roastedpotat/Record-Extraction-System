496bb1b4a4bdf5558062125bc187a751aceff368
Ryan <ryanriyas123@gmail.com>
2026-05-05 00:02:55 +0400

    Update: changed column names provided without affecting dataframe

--- ExcelFunctions/OpenExcel.py
+++ ExcelFunctions/OpenExcel.py
@@ -4,9 +4,10 @@ def load_in_file(filepath, sheet_name):
     df = pd.read_excel(filepath, sheet_name, engine= "calamine")
     df = df.drop(columns= ["         ", "Unnamed: 36", "Unnamed: 37", 
                        "Unnamed: 38", "Unnamed: 39", "Unnamed: 40", 
-                       "Name", "Age", "DATE OF BIRTH"])
+                       "Name"])
     return df
 
 def get_column_names(df):
+    df = df.drop(columns= ['Comp ID', 'Gender'])
     columns = df.columns.str.strip().str.lower().to_list()
     return columns
